184090a02648fbb91158fedcbdd5e837f7c6bb34
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-09-11 11:49:59 +0200

    test production cors fix

--- api/server.js
+++ api/server.js
@@ -11,7 +11,23 @@ const { PORT, MONGODB_URL } = process.env
 mongoose.connect(MONGODB_URL)
   .then(() => {
     const api = express()
-    api.use(cors())
+
+    // Configurar CORS con opciones específicas
+    const allowedOrigins = ['http://localhost:5173', 'https://factuclient.netlify.app']
+
+    const corsOptions = {
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true)
+        } else {
+          callback(new Error('Not allowed by CORS'))
+        }
+      },
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+      allowedHeaders: ['Authorization', 'Content-Type']
+    }
+
+    api.use(cors(corsOptions))
 
     api.get("/", (req, res) => {
       res.send("hellow world")
