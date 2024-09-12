0836e0de72fee19ac068cecfa828a4a022a90d4a
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-09-12 11:26:56 +0200

    remove debug console log

--- api/handlers/updateDeliveryNoteDateHandler.js
+++ api/handlers/updateDeliveryNoteDateHandler.js
@@ -7,20 +7,15 @@ const { JWT_SECRET } = process.env
 
 export default ((req, res, next) => {
   try {
-    console.log("Entrando en el handler con:", { headers: req.headers, body: req.body, params: req.params });
-
     const token = req.headers.authorization.slice(7)
-    console.log("Token extraído:", token);
 
     const { date } = req.body
     const { deliveryNoteId } = req.params
 
     jwt.verify(token, JWT_SECRET)
       .then(payload => {
-        console.log("Payload verificado:", payload);
         const { sub: userId } = payload
         try {
-          console.log("Llamando a logic.updateDeliveryNoteDate con:", { userId, deliveryNoteId, date });
           logic.updateDeliveryNoteDate(userId, deliveryNoteId, date)
             .then(() => res.status(200).send())
             .catch(error => next(error))
