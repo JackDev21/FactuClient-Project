ecc04fd19086905e539116a8aac716d9264bd531
Jose A. Canto <jose.a.c.lopez@gmail.com>
2024-09-12 10:31:29 +0200

    update for debug render api

--- api/logic/updateDeliveryNoteDate.js
+++ api/logic/updateDeliveryNoteDate.js
@@ -2,8 +2,9 @@ import validate from "com/validate.js"
 import { User, DeliveryNote } from "../model/index.js"
 import { NotFoundError, SystemError } from "com/errors.js"
 
-
 const updateDateDeliveryNote = (userId, deliveryNoteId, date) => {
+  console.log("Entrando en updateDateDeliveryNote con:", { userId, deliveryNoteId, date });
+
   validate.id(userId, "userId")
   validate.id(deliveryNoteId, "deliveryNoteId")
   validate.date(date, "date")
@@ -11,17 +12,26 @@ const updateDateDeliveryNote = (userId, deliveryNoteId, date) => {
   // Convertir la fecha a un objeto Date
   const [day, month, year] = date.split('/');
   const dateObj = new Date(`${year}-${month}-${day}`)
+  console.log("Fecha convertida a objeto Date:", dateObj);
 
   return User.findById(userId).select("-__v").lean()
-    .catch(error => { throw new SystemError(error.message) })
+    .catch(error => {
+      console.error("Error al buscar usuario:", error.message);
+      throw new SystemError(error.message)
+    })
     .then((user) => {
+      console.log("Usuario encontrado:", user);
       if (!user) {
         throw new NotFoundError("User not found")
       }
 
       return DeliveryNote.findByIdAndUpdate(deliveryNoteId, { date: dateObj }, { new: true }).select("-__v").lean()
-        .catch(error => { throw new SystemError(error.message) })
+        .catch(error => {
+          console.error("Error al actualizar nota de entrega:", error.message);
+          throw new SystemError(error.message)
+        })
         .then(updatedDeliveryNote => {
+          console.log("Nota de entrega actualizada:", updatedDeliveryNote);
           if (!updatedDeliveryNote) {
             throw new NotFoundError("Delivery note not found")
           }
@@ -33,4 +43,4 @@ const updateDateDeliveryNote = (userId, deliveryNoteId, date) => {
 
 }
 
-export default updateDateDeliveryNote 
+export default updateDateDeliveryNote
